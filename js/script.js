3e1ca4652acd13481c268b5ac991c363c2aa846a
Adrian Wasylik <p27wasylik@gmail.com>
2022-10-27 17:14:11 +0200

    add remove buttons

--- js/script.js
+++ js/script.js
@@ -10,35 +10,52 @@
         },
     ];
 
-    const render = () => {
-        let htmlString = "";
-
-        for (const task of tasks) {
-            htmlString += `
-                <li
-                    ${task.done ? " class=\"section__li--done\"" : ""}
-                    >
-                    ${task.content}
-                </li>
-            `;
-        }
-
-        document.querySelector(".js-tasksList").innerHTML = htmlString;
-    };
-
     const addNewTask = (newTaskContent) => {
         tasks.push({
             content: newTaskContent,
         });
 
         render();
-    }
+    };
+
+    const removeTask = (index) => {
+        tasks.splice(index, 1);
+        render();
+    };
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+                
+                <li
+                    ${task.done ? " class=\"section__li--done\"" : ""}
+                    >
+                    <button class="section__button--delete js-delete">Usuń</button>
+                    ${task.content}
+                </li>
+            `;
+        }
+
+        document.querySelector(".js-tasksList").innerHTML = htmlString;
+
+        const removeButtons = document.querySelectorAll(".js-delete");
+
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
+            });
+        });
+    };
+
+
 
     const onFormSubmit = (event) => {
         event.preventDefault();
 
         newTaskContent = document.querySelector(".js-newTask").value.trim();
-        
+
         if (newTaskContent === "") {
             return;
         }
